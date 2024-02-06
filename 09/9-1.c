508609247b7b1f1157aa0a3374890bca1b560435
hayeong120 <jbg01200249@gmail.com>
2024-02-06 22:10:34 +0900

    Chapter 09-1

--- 09/9-1.c
+++ 09/9-1.c
@@ -6,7 +6,7 @@ int main(void)
 	double b;
 	char c;
 
-	printf("int형 변수의 주소 : %u\n", &a);
+	printf("int형 변수의 주소 : %p\n", &a);
 	printf("double형 변수의 주소 : %u\n", &b);
 	printf("char형 변수의 주소 : %u\n", &c);
 
